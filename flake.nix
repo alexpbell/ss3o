86f8ce34e6fbdca48039d82f9f5a332a03cb99a3
Alexander Campbell <ghostofsandy@gmail.com>
2024-07-28 13:32:45 +1000

    local files

--- flake.nix
+++ flake.nix
@@ -51,41 +51,25 @@
           cat ss3/SS_biofxn.tpl ss3/SS_miscfxn.tpl ss3/SS_selex.tpl ss3/SS_popdyn.tpl ss3/SS_recruit.tpl ss3/SS_benchfore.tpl ss3/SS_expval.tpl ss3/SS_objfunc.tpl ss3/SS_write.tpl ss3/SS_write_ssnew.tpl ss3/SS_write_report.tpl ss3/SS_ALK.tpl ss3/SS_timevaryparm.tpl ss3/SS_tagrecap.tpl > SS_functions.temp
           cat ss3/SS_versioninfo_330safe.tpl ss3/SS_readstarter.tpl ss3/SS_readdata_330.tpl ss3/SS_readcontrol_330.tpl ss3/SS_param.tpl ss3/SS_prelim.tpl ss3/SS_global.tpl ss3/SS_proced.tpl SS_functions.temp > ss3.tpl
           ./tpl2cpp ss3
-          mkdir -p admb/include
-          cp admb/src/nh99/*.h admb/include
-          cp admb/src/nh99/evalxtrn.cpp admb/include
-          rm admb/src/nh99/evalxtrn.cpp
-          cp admb/src/linad99/*.hpp admb/include
-          cp admb/src/linad99/*.h admb/include
-          cp admb/src/tools99/*.hpp admb/include
-          cp admb/src/tools99/*.h admb/include
-          cp admb/src/tools99/integrate.cpp admb/include
-          rm admb/src/tools99/integrate.cpp
-          cp admb/src/df1b2-separable/*.h admb/include
-          rm admb/src/linad99/getopt.cpp
-          sed "s/std::scientific < setp/std::scientific << std::setp/g" admb/src/linad99/xfmmtr1.cpp > admb/src/linad99/xfmmtr1_fix.cpp
-          rm admb/src/linad99/xfmmtr1.cpp
-          mv admb/src/linad99/xfmmtr1_fix.cpp admb/src/linad99/xfmmtr1.cpp        
-          sed 's/#include "tweedie_logW.cpp"//g' admb/src/linad99/dtweedie.cpp > admb/src/linad99/fix.cpp
-          cat admb/src/linad99/tweedie_logW.cpp >> admb/src/linad99/fix.cpp
-          rm admb/src/linad99/dtweedie.cpp
-          rm admb/src/linad99/tweedie_logW.cpp
-          mv admb/src/linad99/fix.cpp dtweedie.cpp
-          clang++ -c -g admb/src/linad99/*.cpp -Iadmb/include -D_USE_MATH_DEFINES
-          mv expm.o linexpm.o
-          clang++ -c -g admb/src/tools99/*.cpp -Iadmb/include -D_USE_MATH_DEFINES       
-          clang++ -c -g admb/src/nh99/*.cpp -Iadmb/include -D_USE_MATH_DEFINES       
-          clang++ -c -g admb/src/df1b2-separable/*.cpp -Iadmb/include -D_USE_MATH_DEFINES
-          clang++ -c -g admb/src/sparse/*.cpp -Iadmb/include -D_USE_MATH_DEFINES
-          sed "s/abs(\(.*parm_1(j, 8) > 0\))/\1/g" ss3.cpp > ss3_fix.cpp
-          rm ss3.cpp
-          mv ss3_fix.cpp ss3.cpp
-          sed -e '/#include <ss3.htp>/rss3.htp' ss3.cpp > ss3_fix.cpp
-          sed "s/#include <ss3.htp>//g" ss3_fix.cpp > ss3.cpp
+          cp admb/src/nh99/*.h .
+          cp admb/src/nh99/evalxtrn.cpp .
+          cp admb/src/linad99/*.hpp .
+          cp admb/src/linad99/*.h .
+          cp admb/src/tools99/*.hpp .
+          cp admb/src/tools99/*.h .
+          cp admb/src/tools99/integrate.cpp .
+          cp admb/src/df1b2-separable/*.h .
+          sed "s/std::scientific < setp/std::scientific << std::setp/g" xfmmtr1.cpp > xfmmtr1.cpp
+          sed 's/#include "tweedie_logW.cpp"//g' dtweedie.cpp > dtweedie.cpp
+          cat tweedie_logW.cpp >> dtweedie.cpp
+          rm tweedie_logW.cpp
+          sed "s/abs(\(.*parm_1(j, 8) > 0\))/\1/g" ss3.cpp > ss3.cpp
+          sed -e '/#include <ss3.htp>/rss3.htp' ss3.cpp > ss3.cpp
+          sed "s/#include <ss3.htp>//g" ss3.cpp > ss3.cpp
           rm ss3.htp
-          rm ss3_fix.cpp
-          clang++ -c -g ss3.cpp -Iadmb/include -D_USE_MATH_DEFINES
-          clang++ -g *.o -Iadmb/include/ -D_USE_MATH_DEFINES -o ss3o
+          mv expm.o linexpm.o
+          clang++ -c -g *.cpp -D_USE_MATH_DEFINES
+          clang++ -g *.o -D_USE_MATH_DEFINES -o ss3o
       '';
 
         installPhase = ''
